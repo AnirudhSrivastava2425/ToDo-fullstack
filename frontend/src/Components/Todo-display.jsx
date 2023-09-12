aaa76749dc461b7467d8f69a449b95b086253300
AnirudhSrivastava2425 <91976754+AnirudhSrivastava2425@users.noreply.github.com>
2023-09-12 22:11:41 +0530

    displaying todos on frontend

--- frontend/src/Components/Todo-display.jsx
+++ frontend/src/Components/Todo-display.jsx
@@ -11,10 +11,22 @@ function Todo() {
     result = await result.json();
     setTodo(result);
   }
+  console.log(todo);
   return (
     <div className='all-todos'>
         <div className="todos" id='does'>
-          
+          {
+            todo.map((item,index) => 
+              <div className="cards-todo">
+                <h2 className='titles'>{item.Title}</h2>
+                <p className='desc'>{item.Description}</p>
+                <div>
+
+                <button className='btn btn-outline-secondary'>Done</button>
+                </div>
+              </div>
+            )
+          }
         </div>
         <div className="todos" id='done'>done</div>
     </div>
